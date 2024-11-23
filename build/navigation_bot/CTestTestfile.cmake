1734388eb99d76baa066013c8947d73aed095224
NguynnQuanggTruongg12 <166543877+NguynnQuanggTruongg12@users.noreply.github.com>
2024-11-23 09:38:05 +0700

    upload source code

---
+++ build/navigation_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/catkin_ws/src/navigation_bot
+# Build directory: /home/robot/catkin_ws/build/navigation_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/gennodejs/ros/slamware_ros_sdk/msg/_index.js
@@ -0,0 +1,112 @@
+
+"use strict";
+
+let RobotBasicState = require('./RobotBasicState.js');
+let OptionalBool = require('./OptionalBool.js');
+let RotateToRequest = require('./RotateToRequest.js');
+let RectInt32 = require('./RectInt32.js');
+let Vec2DInt32 = require('./Vec2DInt32.js');
+let MoveToLocationsRequest = require('./MoveToLocationsRequest.js');
+let BasicSensorValueDataArray = require('./BasicSensorValueDataArray.js');
+let BasicSensorInfoArray = require('./BasicSensorInfoArray.js');
+let RecoverLocalizationRequest = require('./RecoverLocalizationRequest.js');
+let ImpactType = require('./ImpactType.js');
+let CancelActionRequest = require('./CancelActionRequest.js');
+let OptionalUInt64 = require('./OptionalUInt64.js');
+let RotateRequest = require('./RotateRequest.js');
+let ActionDirection = require('./ActionDirection.js');
+let MoveLinesRequest = require('./MoveLinesRequest.js');
+let MapKind = require('./MapKind.js');
+let RobotDeviceInfo = require('./RobotDeviceInfo.js');
+let SetMapLocalizationRequest = require('./SetMapLocalizationRequest.js');
+let Line2DFlt32 = require('./Line2DFlt32.js');
+let MoveByThetaRequest = require('./MoveByThetaRequest.js');
+let OptionalUInt32 = require('./OptionalUInt32.js');
+let MoveOptions = require('./MoveOptions.js');
+let BasicSensorValue = require('./BasicSensorValue.js');
+let Line2DFlt32Array = require('./Line2DFlt32Array.js');
+let OptionalUInt8 = require('./OptionalUInt8.js');
+let OptionalInt16 = require('./OptionalInt16.js');
+let LocalizationMovement = require('./LocalizationMovement.js');
+let BasicSensorInfo = require('./BasicSensorInfo.js');
+let AddLineRequest = require('./AddLineRequest.js');
+let OptionalInt8 = require('./OptionalInt8.js');
+let RemoveLineRequest = require('./RemoveLineRequest.js');
+let ArtifactUsage = require('./ArtifactUsage.js');
+let AddLinesRequest = require('./AddLinesRequest.js');
+let LocalizationOptions = require('./LocalizationOptions.js');
+let OptionalFlt32 = require('./OptionalFlt32.js');
+let MoveOptionFlag = require('./MoveOptionFlag.js');
+let MoveToRequest = require('./MoveToRequest.js');
+let OptionalInt64 = require('./OptionalInt64.js');
+let MoveByDirectionRequest = require('./MoveByDirectionRequest.js');
+let OptionalLocalizationMovement = require('./OptionalLocalizationMovement.js');
+let Vec2DFlt32 = require('./Vec2DFlt32.js');
+let GoHomeRequest = require('./GoHomeRequest.js');
+let OptionalInt32 = require('./OptionalInt32.js');
+let OptionalUInt16 = require('./OptionalUInt16.js');
+let RectFlt32 = require('./RectFlt32.js');
+let MoveLineRequest = require('./MoveLineRequest.js');
+let BasicSensorValueData = require('./BasicSensorValueData.js');
+let SyncMapRequest = require('./SyncMapRequest.js');
+let ClearLinesRequest = require('./ClearLinesRequest.js');
+let SensorType = require('./SensorType.js');
+let SetMapUpdateRequest = require('./SetMapUpdateRequest.js');
+let OptionalFlt64 = require('./OptionalFlt64.js');
+let ClearMapRequest = require('./ClearMapRequest.js');
+
+module.exports = {
+  RobotBasicState: RobotBasicState,
+  OptionalBool: OptionalBool,
+  RotateToRequest: RotateToRequest,
+  RectInt32: RectInt32,
+  Vec2DInt32: Vec2DInt32,
+  MoveToLocationsRequest: MoveToLocationsRequest,
+  BasicSensorValueDataArray: BasicSensorValueDataArray,
+  BasicSensorInfoArray: BasicSensorInfoArray,
+  RecoverLocalizationRequest: RecoverLocalizationRequest,
+  ImpactType: ImpactType,
+  CancelActionRequest: CancelActionRequest,
+  OptionalUInt64: OptionalUInt64,
+  RotateRequest: RotateRequest,
+  ActionDirection: ActionDirection,
+  MoveLinesRequest: MoveLinesRequest,
+  MapKind: MapKind,
+  RobotDeviceInfo: RobotDeviceInfo,
+  SetMapLocalizationRequest: SetMapLocalizationRequest,
+  Line2DFlt32: Line2DFlt32,
+  MoveByThetaRequest: MoveByThetaRequest,
+  OptionalUInt32: OptionalUInt32,
+  MoveOptions: MoveOptions,
+  BasicSensorValue: BasicSensorValue,
+  Line2DFlt32Array: Line2DFlt32Array,
+  OptionalUInt8: OptionalUInt8,
+  OptionalInt16: OptionalInt16,
+  LocalizationMovement: LocalizationMovement,
+  BasicSensorInfo: BasicSensorInfo,
+  AddLineRequest: AddLineRequest,
+  OptionalInt8: OptionalInt8,
+  RemoveLineRequest: RemoveLineRequest,
+  ArtifactUsage: ArtifactUsage,
+  AddLinesRequest: AddLinesRequest,
+  LocalizationOptions: LocalizationOptions,
+  OptionalFlt32: OptionalFlt32,
+  MoveOptionFlag: MoveOptionFlag,
+  MoveToRequest: MoveToRequest,
+  OptionalInt64: OptionalInt64,
+  MoveByDirectionRequest: MoveByDirectionRequest,
+  OptionalLocalizationMovement: OptionalLocalizationMovement,
+  Vec2DFlt32: Vec2DFlt32,
+  GoHomeRequest: GoHomeRequest,
+  OptionalInt32: OptionalInt32,
+  OptionalUInt16: OptionalUInt16,
+  RectFlt32: RectFlt32,
+  MoveLineRequest: MoveLineRequest,
+  BasicSensorValueData: BasicSensorValueData,
+  SyncMapRequest: SyncMapRequest,
+  ClearLinesRequest: ClearLinesRequest,
+  SensorType: SensorType,
+  SetMapUpdateRequest: SetMapUpdateRequest,
+  OptionalFlt64: OptionalFlt64,
+  ClearMapRequest: ClearMapRequest,
+};
